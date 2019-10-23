d5e224736d5f005999b6c6f4fdf8f853044a8f00
Yurii Torbyk <mail@yurii.me>
2019-10-23 22:39:34 +0300

    Release 0.12.1

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     | number MAJOR.MINOR.PATCH when an update happens: https://semver.org.
     |
     */
-    'version' => '0.12.0',
+    'version' => '0.12.1',
 
     /*
     |--------------------------------------------------------------------------
